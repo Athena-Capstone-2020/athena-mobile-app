6c898ca0b0c277544e5fbe62dbc02f9e4212eeae
rajeev <rajeev.goonie98@gmail.com>
2020-10-10 12:26:09 -0700

    create temporary boiler plate for food item service

---
+++ src/services/fooditem/FoodItemService.js
@@ -0,0 +1,17 @@
+import { BaseService } from "../base";
+
+export class FoodItemService extends BaseService {
+    
+    getContainersWithItem(){
+        throw new Error('Not Implemented')
+    }
+
+    getGroceryListWithItem(){
+        throw new Error('Not Implemented')
+    }
+
+    getRecipesWithItem(){
+        throw new Error('Not Implemente')
+    }
+
+}
