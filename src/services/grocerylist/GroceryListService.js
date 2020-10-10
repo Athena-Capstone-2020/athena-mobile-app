2a6ec75f21815d58170ed99d620135e2d67f673a
rajeev <rajeev.goonie98@gmail.com>
2020-10-10 12:22:13 -0700

    create boiler plate for grocery list service

---
+++ src/services/grocerylist/GroceryListService.js
@@ -0,0 +1,29 @@
+import { BaseService } from "../base";
+
+export class GroceryListService extends BaseService{
+
+    createGroceryList(){
+        throw new Error('Not Implemented')
+    }
+
+    deleteGroceryList(){
+        throw new Error('Not Implemented')
+    }
+
+    addItem(){
+        throw new Error('Not Implemented')
+    }
+
+    updateItem(){
+        throw new Error('Not Implemented')
+    }
+
+    removeItem(){
+        throw new Error('Not Implemented')
+    }
+
+    getItem(){
+        throw new Error('Not Implemented')
+    }
+
+}
