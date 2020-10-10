b568988042fe6df0650afa6a6912b60d488e60ab
ynigoreyes <ynigoreyes@gmail.com>
2020-10-10 13:49:10 -0500

    adding documentation to Document class

--- src/models/Document.js
+++ src/models/Document.js
@@ -1,4 +1,7 @@
 export class Document {
+    /**
+     * Turns the class into an object that you can save into Firebase directly
+     */
     toDocument() {
         throw new Error('No implementation found')
     }
