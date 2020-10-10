c2fbbdaf03b0b387e8351a2a3acf16fad28d6e03
ynigoreyes <ynigoreyes@gmail.com>
2020-10-10 13:22:11 -0500

    adding base service to inherit from

---
+++ src/services/base/BaseService.js
@@ -0,0 +1,19 @@
+export class BaseService {
+    constructor() {}
+
+    GetById(id) {
+        return new Error('Not implemented')
+    }
+
+    UpdateById(id, changes) {
+        return new Error('Not implemented')
+    }
+
+    CreateEntity(id) { 
+        return new Error('Not implemented')
+    }
+
+    DeleteEntityById(id) {
+        return new Error('Not implemented')
+    }
+}
