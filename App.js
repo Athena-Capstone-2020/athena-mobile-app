6cd935970eab61ae761277c5c1cd50e6a49e2563
Elias Moreno <eliasmoreno@Elias.local>
2020-10-10 17:54:54 -0500

    implemented initial navbar

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from './Provider';
-import { TestButton } from './src/components/TestButton'
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Provider } from "./Provider";
+import Navbar from './src/screens/Navbar';
 
 export default function App() {
   return (
-      <Provider>
-        <View style={styles.container}>
-          <Text>Athena App!</Text>
-          <StatusBar style="auto" />
-          <TestButton />
-        </View>
-      </Provider>
+    <Provider>
+      <Navbar />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
