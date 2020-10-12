2ea58f8be2e9c67023f933d9359fd1d78f7214a7
rajeev <rajeev.goonie98@gmail.com>
2020-10-12 10:48:01 -0700

    adding quantity to foodItem model

--- src/models/FoodItem.js
+++ src/models/FoodItem.js
@@ -2,15 +2,22 @@ import {Document} from "./Document"
 
 export class FoodItem extends Document{
 
-    constructor(name, photoURI){
+    /**
+     * @param {string} name 
+     * @param {*} photoURI 
+     * @param {string} quantity 
+     */
+    constructor(name, photoURI, quantity){
         this.name = name
         this.photoURI = photoURI
+        this.quantity = quantity
     }
 
     toDocument(){
         return {
             name: this.name,
-            photoURI: this.photoURI
+            photoURI: this.photoURI,
+            quantity: this.quantity
         }
     }
 
