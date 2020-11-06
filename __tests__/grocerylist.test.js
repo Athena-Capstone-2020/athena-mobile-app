831ebad5d442a00df4832e53461f895d8ca5d536
rajeev <rajeev.goonie98@gmail.com>
2020-11-05 17:01:26 -0800

    added functionality and tests for getGroceryList and deleteGroceryList

--- __tests__/grocerylist.test.js
+++ __tests__/grocerylist.test.js
@@ -23,7 +23,78 @@ test('should create grocery list without errors', async () => {
 })
 
 //getGroceryListById
-//deleteGroceryListById
+test('should get a grocery list that has been created already without errors', async () => {
+    const [groceryListService] = setup()
+
+    const groceryListCreated = await groceryListService.createGroceryList('getGroceryListByIdTest1', 'someOwnerId')
+    const retrievedGroceryList = await groceryListService.getGroceryListById(groceryListCreated.id)
+
+    expect(retrievedGroceryList).toMatchObject(groceryListCreated)
+})
+
+test('should try to get a grocery list that does not exist and return null without error', async () => {
+    const [groceryListService] = setup()
+
+    const groceryListId = 'I_DO_NOT_EXIST'
+    const retrievedGroceryList = await groceryListService.getGroceryListById(groceryListId)
+
+    expect(retrievedGroceryList).toBe(null)
+})
+
+//deleteGroceryList
+test('should create and delete a grocery list by id without error', async () => {
+    const [groceryListService] = setup()
+
+    const newGroceryList = await groceryListService.createGroceryList('toBeDeleted', 'someOwnerId')
+    const groceryListCreated = await groceryListService.getGroceryListById(newGroceryList.id)
+    expect(groceryListCreated).toMatchObject(newGroceryList)
+
+    const deletedGroceryList = await groceryListService.deleteGroceryListById(newGroceryList.id)
+    expect(deletedGroceryList).toMatchObject(newGroceryList)
+
+    const groceryListAfterDeletion = await groceryListService.getGroceryListById(newGroceryList.id)
+    expect(groceryListAfterDeletion).toBe(null)
+})
+
+test('should try to delete grocery list by id that does not exist and returns null', async () => {
+    const [groceryListService] = setup()
+
+    const groceryListId = 'I_DO_NOT_EXIST'
+    const deletedGroceryList = await groceryListService.deleteGroceryListById(groceryListId)
+    expect(deletedGroceryList).toBe(null)
+
+})
+
+test('should create and delete a grocery list by object without error', async () => {
+    const [groceryListService] = setup()
+
+    const newGroceryList = await groceryListService.createGroceryList('toBeDeleted', 'someOwnerId')
+    const groceryListCreated = await groceryListService.getGroceryListById(newGroceryList.id)
+    expect(groceryListCreated).toMatchObject(newGroceryList)
+
+    const deletedGroceryList = await groceryListService.deleteGroceryListByObject(newGroceryList)
+    expect(deletedGroceryList).toMatchObject(newGroceryList)
+
+    const groceryListAfterDeletion = await groceryListService.getGroceryListById(newGroceryList.id)
+    expect(groceryListAfterDeletion).toBe(null)
+})
+
+test('attemps to delete a container by objec that has a null is and errors', async () => {
+    const [groceryListService] = setup()
+
+    const groceryList = new GroceryList('toBeDeleted', 'someOwnerId', [])
+
+    let errorCaught = null
+    try{
+        const deletedGroceryList = await groceryListService.deleteGroceryListByObject(groceryList)
+    }
+    catch(err){
+        errorCaught = err
+    }
+
+    expect(errorCaught).toStrictEqual(new Error('the grocery list does not have an id'))
+})
+
 //addItemToGroceryList
 //updateItemInGroceryList
 //removeItemFromGroceryList
--- src/services/grocerylist/GroceryListService.js
+++ src/services/grocerylist/GroceryListService.js
@@ -7,7 +7,7 @@ export class GroceryListService extends BaseService{
     GROCERY_LIST_COLLECTION = 'GROCERY_LIST_COLLECTION'
 
     /**
-     * Creates a new grocery list
+     * Creates a new grocery list and returns the grocery list object with an id
      * @param {string} name name of the grocery list
      * @param {string} ownerId the household/person the container belongs to
      * @returns the groceryList object that was created
@@ -29,11 +29,75 @@ export class GroceryListService extends BaseService{
     }
 
     /**
-     * Deletes a specified grocery list
-     * @param {string} id 
+     * Gets a grocery list that has been created. If the grocery list does not exist, a null will be returned
+     * @param {string} id id of the grocery list trying to be retrieved
+     * @returns a grocery list object or null if not found
+     * @throws error if id is null 
      */
-    deleteGroceryListById(id){
-        throw new Error('Not Implemented')
+    async getGroceryListById(id){
+        try{
+            this.__UseCollection(this.GROCERY_LIST_COLLECTION)
+            if(id == null)
+                throw new Error("id cannot be null")
+
+            const groceryListDoc = await this.__GetById(id)
+
+            if(groceryListDoc == undefined)
+                return null
+
+            const groceryList = new GroceryList(groceryListDoc.name, groceryListDoc.ownerId, groceryListDoc.foodItems)
+            groceryList.id = id
+
+            return groceryList
+        }
+        catch(err){
+            logError(err)
+            throw err
+        }
+    }
+
+    /**
+     * Attempts to delete a grocery list by using its object id
+     * @param {GroceryList} groceryList the grocery list to be deleted
+     * @returns the grocery list obj that was deleted, or null if the grocery list was not found in the DB
+     * @throws error if grocery list id is null 
+     */
+    async deleteGroceryListByObject(groceryList){
+        try{
+            this.__UseCollection(this.GROCERY_LIST_COLLECTION)
+
+            const groceryListId = groceryList.id
+            if( groceryListId == null )
+                throw new Error('the grocery list does not have an id')
+
+            return await this.deleteGroceryListById(groceryListId)
+        }
+        catch(err){
+            logError(err)
+            throw err
+        }
+    }
+
+    /**
+     * Attemps to delete a grocery list by its id
+     * @param {string} id the id of the grocery list to be deleted
+     * @returns the container obj that was deleted, or null if the container was not found
+     */
+    async deleteGroceryListById(id){
+        try{
+            this.__UseCollection(this.GROCERY_LIST_COLLECTION)
+
+            const groceryListToDelete = await this.getGroceryListById(id)
+            if( groceryListToDelete == null )
+                return null
+            
+            await this.__DeleteEntityById(id)
+            return groceryListToDelete
+        }
+        catch(err){
+            logError(err)
+            throw err
+        }
     }
 
     /**
