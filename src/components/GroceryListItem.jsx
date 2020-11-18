cb45bbb9c6a6e36732910ba2fc6bfde0cb679364
BlaineSieck <bsieck14@gmail.com>
2020-11-17 21:28:12 -0600

    Added scrolling to list items

--- src/components/GroceryListItem.jsx
+++ src/components/GroceryListItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Dimensions } from 'react-native'
+import { StyleSheet, Dimensions, ScrollView } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 import CheckBoxButton from '../components/CheckBoxButton';
 import IconButton from '../components/IconButton';
@@ -7,9 +7,11 @@ import { Box, Text } from '../components/index';
 import Svg, { G, Path, Rect, TSpan } from "react-native-svg";
 
 const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
     groceryList: {
+        height: windowHeight - windowHeight*0.25,
         paddingTop: 17,
         alignSelf: "flex-start"
      },
@@ -25,7 +27,7 @@ const styles = StyleSheet.create({
     },
     addItem: {
        paddingLeft: 40,
-       paddingTop: 350
+       paddingTop: 20
     }
 });
 
@@ -43,26 +45,28 @@ const HamburgerIcon = (props) => {
     )
 }
 
-const listItems = ["Hot Dogs", "Bananas", "Red Onions", "Bacon"];
+const listItems = ["Hot Dogs", "Bananas", "Red Onions", "Bacon", "Tacos", "Mustard", "Bread", "Ranch", "Salad", "Brownie Mix", "Chocolate Chips", "Milk", "Eggs"];
 
 const GroceryListItem = ({ style }) => {
     return (
         <Box style={[styles.groceryList, style ? style : {}]}>
-            {listItems.map((item, key) => {
-                return (
-                    <Box style={styles.container} flexDirection="row" key={key}>
-                        <Box flexDirection="row">
-                            <CheckBoxButton style={styles.checkBox}/>
-                            <Box flexDirection="column">
-                                <Text variant="groceryListItem" style={styles.item}>{item}</Text>
+            <ScrollView showsVerticalScrollIndicator ={false}>
+                {listItems.map((item, key) => {
+                    return (
+                        <Box style={styles.container} flexDirection="row" key={key}>
+                            <Box flexDirection="row">
+                                <CheckBoxButton style={styles.checkBox}/>
+                                <Box flexDirection="column">
+                                    <Text variant="groceryListItem" style={styles.item}>{item}</Text>
+                                </Box>
                             </Box>
+                            <HamburgerIcon style={styles.hamburger}/>
                         </Box>
-                        <HamburgerIcon style={styles.hamburger}/>
-                    </Box>
-                )
-            })}
+                    )
+                })}
+            </ScrollView>
             <IconButton variant="addItem" style={styles.addItem}></IconButton>
-        </Box>
+        </Box>  
     )
     
 }
