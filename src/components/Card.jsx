91f1521d736543a0762b4406791bf3fa31e8c385
athena-capstone <72465071+athena-capstone@users.noreply.github.com>
2020-11-17 19:22:52 -0800

    adding card (#37)
    Co-authored-by: ynigoreyes <ynigoreyes@gmail.com>

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Image } from 'react-native'
+import { Box, Text } from './Theme'
+
+/**
+ * @param {{ imageUri: string, bottomContent: React.Component, width: string | number, height: string | number }} props 
+ */
+export function Card(props) {
+    const style = {
+        container: {
+            backgroundColor: 'white',
+            borderRadius: 10,
+            width: props.width || 150,
+            minHeight: props.height || 75,
+            shadowColor: '#000',
+            shadowOffset: { width: 0, height: 5 },
+            shadowOpacity: 0.3,
+            shadowRadius: 4,
+        },
+        bottomContainer: {
+            justifyContent: 'center',
+            alignItems: 'center'
+        }
+    }
+
+    return (
+        <Box style={style.container}>
+            <Image source={{ uri: props.imageUri }} style={{width: '100%', minHeight: 100 }} />
+            <Box style={style.bottomContainer}>
+            {props.bottomContent}
+            </Box>
+        </Box>
+    )
+}
