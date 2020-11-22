bd4c47ab0894419b4b5767a5be19730fef05f606
Ynigo Reyes <26727346+ynigoreyes@users.noreply.github.com>
2020-11-22 17:38:23 -0600

    adding setUser method (#50)

--- src/global/user-context/state/reducer.js
+++ src/global/user-context/state/reducer.js
@@ -3,11 +3,15 @@ import UserContextActionTypes from './actions'
 export function reducer(state, action) {
     switch (action.type) {
         case (UserContextActionTypes.FETCH_HOUSEHOLD):
-            return { household: null, householdMembers: [] }
+            return { household: null, householdMembers: [], user: state.user }
         case (UserContextActionTypes.SET_HOUSEHOLD):
-            return { household: action.payload.household, householdMembers: action.payload.householdMembers }
+            return { household: action.payload.household, householdMembers: action.payload.householdMembers, user: state.user }
+        case (UserContextActionTypes.FETCH_USER):
+            return { ...state, user: null }
+        case (UserContextActionTypes.SET_USER):
+            return { ...state, user: action.payload.user }
         case (UserContextActionTypes.ERROR_OUT):
-            return { household: null, householdMembers: [] }
+            return { household: null, householdMembers: [], user: state.user }
         default:
             throw new Error(`Unhandled action: ${action} with current state of: ${state}`)
     }
