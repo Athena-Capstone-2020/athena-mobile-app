151464e6a9a31cbe913c769db0aaa1e27555275f
rajeev <rajeev.goonie98@gmail.com>
2020-12-01 10:25:23 -0800

    removed in app warnings for demo

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, LogBox } from "react-native";
 import { Provider } from "./Provider";
 import { LoadAssets } from "./src/components/index";
 import { ThemeProvider } from './src/components/Theme'
 import Athena from './src/screens/Athena'
 
 export default function App() {
+  LogBox.ignoreLogs(['Warning: ...'])
+  LogBox.ignoreAllLogs()
+
   return (
     <Provider>
       <ThemeProvider>
